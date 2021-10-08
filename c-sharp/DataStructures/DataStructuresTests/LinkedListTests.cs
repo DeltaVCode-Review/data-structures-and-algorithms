1082262af383734bb5267bb2ab3b07a720904ab1
mistidinzy <mistidinzy@gmail.com>
2021-10-08 15:55:06 -0500

    test passing

--- c-sharp/DataStructures/DataStructuresTests/LinkedListTests.cs
+++ c-sharp/DataStructures/DataStructuresTests/LinkedListTests.cs
@@ -34,8 +34,14 @@ namespace DataStructuresTests
       list.Insert(1);
 
       // Assert
+      // make sure we have a head
       Assert.NotNull(list.Head);
+
+      //make sure head node has the inserted value
       Assert.Equal(1, list.Head.Value);
+
+      // make sure the head node does not have a next
+      Assert.Null(list.Head.Next);
     }
 
   }
